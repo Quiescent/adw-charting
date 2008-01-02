758d2b4fd75dc8055e6e521101300482ccd8d13b
Ryan Davis <ryan@acceleration.net>
2008-01-02 16:03:47 -0500

    tweak drawing of lines so it stays within bounds
    darcs-hash:20080102210347-ac9f3-bb45e59976850f86c8f1e593dcf24b55695ffa06.gz

--- src/line-charts.lisp
+++ src/line-charts.lisp
@@ -31,8 +31,9 @@
 
     ;;draw the graphs
     (set-rgb-fill .9 .9 .9)
-    (rectangle graph-x graph-y graph-width graph-height)
-    (fill-path)
+    (set-rgb-stroke 0 0 0)
+    (rectangle (1- graph-x) (1- graph-y) (1+ graph-width) (1+ graph-height))
+    (fill-and-stroke)
 
     ;;figure out the right scaling factors so we fill the graph    
     ;find the min/max x/y across all series
@@ -44,41 +45,61 @@
 	   (min-y (cadar extremes))
 	   (max-x (caadr extremes))
 	   (max-y (cadadr extremes))
+	   (gx graph-x)
+	   (gy graph-y)
 	   (scale-x (/ graph-width (- max-x min-x)))
-	   (scale-y (/ graph-height (1+ (- max-y min-y)))))
-      ;adjust the origins if we need to
+	   (scale-y (/ graph-height (* 1.1 (- max-y min-y)))))
+					;adjust the origins if we need to
       (when (> 0 min-y)
-	(incf graph-y (abs (* scale-y min-y))))
+	(incf gy (abs (* scale-y min-y))))
       (when (> 0 min-y)
-	(incf graph-x (abs (* scale-x min-x))))
-      (flet ((convert-point (x y)
-	       "convert a point from data space to graph space"
-	       (values (+ graph-x (* scale-x x))
-		       (+ graph-y (* scale-y y)))))
+	(incf gx (abs (* scale-x min-x))))
+
+      ;;define our graph bounds
+      (let ((max-graph-x (truncate (+ graph-x graph-width)))
+	    (min-graph-x graph-x)
+	    (max-graph-y (- (truncate (+ graph-y graph-height)) graph-margin))
+	    (min-graph-y (+ (truncate graph-y) graph-margin)))
+	(format *trace-output*
+		"graph bounds: ~a ~a, ~a ~a~%"
+		min-graph-x min-graph-y max-graph-x max-graph-y)
+	(flet ((convert-point (x y)
+		 "convert a point from data space to graph space"
+
+		 ;;try to keep the point in our graph bounds
+		 (values (truncate (min (max (+ gx (* scale-x x))
+					     min-graph-x)
+					max-graph-x))
+			 (truncate (min (max (+ gy (* scale-y y))
+					     min-graph-y)
+					max-graph-y)))))
     
     
-	;draw the 0 line
-	(multiple-value-bind (x y) (convert-point min-x 0)
-	  (move-to x y))
-	(multiple-value-bind (x y) (convert-point max-x 0)
-	  (line-to x y))
-	(set-stroke '(0 0 0))
-	(stroke)
-	(set-line-width 2)
-	(mapc
-	 #'(lambda (series)
-	     (with-graphics-state
-	       (set-stroke series)
-	       (let ((drawing-p nil))
-		 (loop for (x y) in (data series)
-		       do (multiple-value-bind (px py) (convert-point x y)
-			    (if drawing-p
-				(line-to px py)
-				(progn
-				  (move-to px py)
-				  (setf drawing-p T))))))
-	       (stroke)))
-	 (series chart)))))
+					;draw the 0 line
+	  (multiple-value-bind (x y) (convert-point min-x 0)
+	    (move-to x y))
+	  (multiple-value-bind (x y) (convert-point max-x 0)
+	    (line-to x y))
+	  (set-stroke '(0 0 0))
+	  (stroke)
+	  (set-line-width 2)
+	  
+	  (mapc
+	   #'(lambda (series)
+	       (with-graphics-state
+		 (set-stroke series)
+		 (let ((drawing-p nil))
+		   (loop for (x y) in (data series)
+			 counting T into i
+			 do (multiple-value-bind (px py) (convert-point x y)
+			      (format *trace-output* "converted ~a: ~a ~a -> ~a ~a~%" i x y px py)
+			      (if drawing-p
+				  (line-to px py)
+				  (progn
+				    (move-to px py)
+				    (setf drawing-p T))))))
+		 (stroke)))
+	   (series chart))))))
   (draw-legend chart))
 
 (defmethod draw-legend ((chart line-chart))
