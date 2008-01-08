e39149b76ac83568f676757ad3b3644499b83249
Ryan Davis <ryan@acceleration.net>
2008-01-08 14:06:22 -0500

    reworked the test file a little bit
    darcs-hash:20080108190622-ac9f3-6b045f03ef6cb6b84738ddaddf91e4c6fd2de960.gz

--- test/charting.lisp
+++ test/charting.lisp
@@ -3,26 +3,21 @@
 
 (in-package #:adw-charting-tests)
 
-(defparameter *pie-chart-data* (list
-				(make-instance 'slice :value 10)
-				(make-instance 'slice :value 15)
-				(make-instance 'slice :value 20)))
-
 (define-test pie-chart-total
   (assert-equal 10 (total (make-instance 'pie-chart :total 10))))
 
 (define-test pie-chart-calculated-total
   "tests summing the pie-chart total from the data items"
-  (assert-equal 45 (total (make-instance 'pie-chart :slices *pie-chart-data*))))
+  (assert-equal 45
+		(total (make-instance 'pie-chart :slices (make-slices '(10 15 20))))))
 
 
 (defun pie-chart-sample ()  
   "draws a simple pie chart, rending to pie-chart-sample.png"
   (render-chart
-   (make-instance 'pie-chart
-		  :slices (make-slices '(40
-					    (10 "baz")
-					    (60 "bar" (.5 .5 .5)))))
+   (make-instance 'pie-chart :slices
+		  (make-slices '((5.0d0 "POP3-First")
+				 (2.0d0 "POP3-Additional"))))
    "pie-chart-sample.png"))
 
 
@@ -30,16 +25,15 @@
 ;;see output at http://ryepup.unwashedmeme.com/files/line-chart-sample.png
 (defun line-chart-sample ()  
   "draws a simple line chart"
-  (let ((seriesA (make-instance 'series
-				:label "SeriesA"
-					;data expressed as x/y pairs
-				:data '((-1 -2) (0 4) (1 5) (4 6) (5 -3))))
-	(seriesB (make-instance 'series
-				:label "SeriesB"
-				:data '((-1 4) (0 -2) (1 6) (5 -2) (6 5)))))
-    (render-chart
-     (make-instance 'line-chart
-		    :width 400
-		    :background '(.7 .7 .7)
-		    :series (list seriesA seriesB))
-     "line-chart-sample.png")))
+  (let* ((seriesA (make-instance 'series
+				 :label "SeriesA"
+					;data expressed as a list (x y) pairs
+				 :data '((-1 -2) (0 4) (1 5) (4 6) (5 -3))))
+	 (seriesB (make-instance 'series
+				 :label "SeriesB"
+				 :data '((-1 4) (0 -2) (1 6) (5 -2) (6 5))))
+	 (chart (make-instance 'line-chart
+			       :width 400
+			       :background '(.7 .7 .7)
+			       :series (list seriesA seriesB))))
+    (render-chart chart "line-chart-sample.png")))
