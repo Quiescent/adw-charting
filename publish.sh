ffa0aefa9f34bd9c22d049c110ff7b5828ec6f28
ryan <ryan@acceleration.net>
2008-01-29 19:21:02 -0500

    adding a publish script, this is probably not useful to anyone but me.
    darcs-hash:20080130002102-18786-b80bf174b5220d34abe457c89d7774d089c9957d.gz

---
+++ publish.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+echo "Compile the help"
+~/clbuild/clbuild lisp --load doc/make-docs.lisp
+echo "Publish the help"
+scp doc/index.html rdavis@common-lisp.net:/project/adw-charting/public_html/
+scp doc/*.png rdavis@common-lisp.net:/project/adw-charting/public_html/
+echo "Make the distribution tarball"
+darcs dist -d adw-charting
+echo "publish the distribution tarball"
+scp adw-charting.tar.gz rdavis@common-lisp.net:/project/adw-charting/public_html/
+echo "all done.  Now go blog and post to the mailing list."
