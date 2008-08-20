c98ccaed1d518c16eea7f3cf04867b20fa56efbc
Ryan Davis <ryan@acceleration.net>
2008-08-20 15:49:08 -0400

    eliminated some duplicated exports
    darcs-hash:20080820194908-ac9f3-5733fa1133f1f14911796ee529e09e6a2037188c.gz

--- src/vecto/packages.lisp
+++ src/vecto/packages.lisp
@@ -1,3 +1,3 @@
 (in-package :adw-charting)
 (use-package 'vecto)
-(export '(with-pie-chart add-slice with-line-chart add-series set-axis with-bar-chart))
+(export '(with-pie-chart with-line-chart with-bar-chart))
