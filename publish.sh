92354af727273106752f5c7bb70a7e77ecca22e1
Ryan Davis <ryan@acceleration.net>
2009-08-25 13:30:13 -0400

    some doc fixes and publish script fixes
    darcs-hash:20090825173013-ac9f3-58ba00af1a84d36cab6cffad0aa2704691f4efe1.gz

--- publish.sh
+++ publish.sh
@@ -3,9 +3,10 @@
 echo "sync with cl-user"
 darcs push rdavis@common-lisp.net:/project/adw-charting/public_html/darcs/adw-charting
 echo "Compile the help"
-
+read -p "hit enter when everything is ready in the doc folder, org files exported, etc"
 echo "Publish the help"
-scp doc/index.html rdavis@common-lisp.net:/project/adw-charting/public_html/
+scp doc/docs.html rdavis@common-lisp.net:/project/adw-charting/public_html/index.html
+scp doc/*.html rdavis@common-lisp.net:/project/adw-charting/public_html/
 scp doc/*.png rdavis@common-lisp.net:/project/adw-charting/public_html/
 echo "Make the distribution tarball"
 darcs dist -d adw-charting
